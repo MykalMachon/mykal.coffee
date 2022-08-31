2daa71741141d99c0ad0a17544ec2dd2eb8e9b78
Mykal Machon <mykal@tinybox.dev>
2022-08-30 23:24:28 -0700

    feat: posts and a new format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
-import { defineConfig } from 'astro/config';
-// integrations
+import { defineConfig } from 'astro/config'; // integrations
+
 import preact from '@astrojs/preact';
 import sitemap from '@astrojs/sitemap';
+import mdx from "@astrojs/mdx";
 
+// https://astro.build/config
 export default defineConfig({
   srcDir: './src',
   publicDir: './public',
   site: 'https://mykal.coffee',
   markdown: {
-    drafts: true,
+    drafts: true
   },
-  integrations: [preact(), sitemap()],
+  integrations: [preact(), sitemap(), mdx()]
 });
